965942a54beb22c040b14ba60c0d6b0d8704fd2e
Raymond <dnomyar924@gmail.com>
2021-11-22 19:04:12 +1030

    first iteration script

--- YAML_format_CLI.py
+++ YAML_format_CLI.py
@@ -10,7 +10,17 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+
+if not args.filename.lower().endswith('.csv'):
+    print("{0} is not a CSV file".format(args.filename))
+    exit()
+
 with open(args.filename, mode='rt') as f:
     read = csv.reader(f, delimiter=',')
     _ = next(read)
-    sorter = sorted(read, key=lambda row: (int(row[3]), int(row[4])))
+    sorter = sorted(read, key=lambda row: (int(row[3]), -int(row[4])))
+
+print("records:")
+for row in sorter[:3]:
+    print("""- name: {0} {1}
+  details: In division {2} from {3} performing {4}""".format(row[0], row[1], row[3], row[2], row[5]))
